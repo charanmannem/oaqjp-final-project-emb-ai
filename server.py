4d1842d2fe37889a096cadaeb920d86a92e47780
charanmannem <charanmannem91@gmail.com>
2026-06-02 03:16:37 -0400

    Created Flask web application

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from EmotionDetection import emotion_detector
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def render_index_page():
+    """
+    Render the main page.
+    """
+    return render_template("index.html")
+
+
+@app.route("/emotionDetector")
+def emotion_detector_route():
+    """
+    Analyze emotions from user text.
+    """
+
+    text_to_analyze = request.args.get("textToAnalyze")
+
+    response = emotion_detector(text_to_analyze)
+
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
+        f"The dominant emotion is "
+        f"{response['dominant_emotion']}."
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
